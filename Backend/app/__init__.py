0a5c87803310811c4746f133fb8cef76c14b68de
Sivan <sivanp27540@gmail.com>
2025-10-06 11:03:20 +0300

    Add health Blueprint and basic Flask app structure

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .health.routes import bp as health_bp
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(health_bp, url_prefix="/")
+
     return app
